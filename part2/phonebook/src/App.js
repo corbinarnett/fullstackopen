59ab6c82f3e1259d4fb3ded9e3b1b3fb9bd18286
Corbin Arnett <40528231+corbinarnett@users.noreply.github.com>
2023-01-27 13:50:30 +0100

    add new handleChange function

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,16 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Chief Dingus" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
 
-  const addPerson = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     const personObject = {
       name: newName,
@@ -23,25 +28,37 @@ const App = () => {
     setNewNumber("");
   };
 
-  const handleNameChange = (event) => {
-    // console.log(event.target.value);
-    setNewName(event.target.value);
-  };
+  const handleChange = (event) => {
+    let inputName = event.target.name;
+    let inputValue = event.target.value;
 
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
+    switch (inputName) {
+      case "name":
+        setNewName(inputValue);
+        break;
+      case "number":
+        setNewNumber(inputValue);
+        break;
+      default:
+    }
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} required />
+          name:{" "}
+          <input name="name" value={newName} onChange={handleChange} required />
         </div>
         <div>
           number:{" "}
-          <input value={newNumber} onChange={handleNumberChange} required />
+          <input
+            name="number"
+            value={newNumber}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div>
           <button type="submit">add</button>
